1e8102efe2f4cf70e72955e4704db3fb48ed4094
George Kaimakis <geoptus@gmail.com>
2019-11-20 20:29:12 +0000

    Alternative publish attempt:
    	modified:   cpu_mem_mqtt.py

--- cpu_mem_mqtt.py
+++ cpu_mem_mqtt.py
@@ -18,8 +18,12 @@ from __future__ import print_function
 import paho.mqtt.publish as publish
 import psutil
 import credentials
+import os
+from time import sleep
+
 
 ###   Variables   ###
+LOOP_INTERVAL = 28.5    # seconds
 
 ###   Start of Channel Settings   ###
 
@@ -55,7 +59,7 @@ useSSLWebsockets = True
 
 
 # The Hostname of the ThingSpeak MQTT service
-mqttHost = "mqtt.thingspeak.com"
+mqttHost = "api.thingspeak.com"
 
 # Set up the connection parameters based on the connection type
 if useUnsecuredTCP:
@@ -75,28 +79,45 @@ if useSSLWebsockets:
     tPort = 443
 
 # Create the topic string
-topic = "channels/" + channelID + "/publish/" + apiKey
+topic = "update?" + "api_key=" + apiKey
+
 
 # Run a loop which calculates the system performance every
 #   xx seconds and publishes that to a ThingSpeak channel
 #   using MQTT.
 
-while(True):
 
-    # get the system performance data:
-    cpuPercent = psutil.cpu_percent(interval=30)
-    ramPercent = psutil.virtual_memory().percent
-    print (" CPU =",cpuPercent,"   RAM =",ramPercent)
+def dots(*, interval=0.5, num_of_dots=3, xtr_line=False):
+    for i in range(num_of_dots):
+        sleep(interval)
+        print(".", end='', flush=True)
+    if xtr_line:
+        print()     # print extra line - leaves dots in place
 
-    # build the payload string:
-    tPayload = "field1=" + str(cpuPercent) + "&field2=" + str(ramPercent)
 
-    # attempt to publish this data to the topic:
-    try:
-        publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
+# clear the screen:
+os.system('clear')
 
-    except (KeyboardInterrupt):
-        break
 
-    except:
-        print ("There was an error while publishing the data.")
+try:
+    while(True):
+        dots()
+        # get the system performance data:
+        cpuPercent = psutil.cpu_percent(interval=0.5)
+        ramPercent = psutil.virtual_memory().percent
+        print ("\rCPU usage = {}%".format(cpuPercent),"\tRAM usage = {}%".format(ramPercent))
+
+        # build the payload string:
+        tPayload = "&field1=" + str(cpuPercent) + "&field2=" + str(ramPercent)
+
+        # attempt to publish this data to the topic:
+        try:
+            publish.single(topic, payload=tPayload, hostname=mqttHost) # , \
+#                    port=tPort, tls=tTLS, transport=tTransport)
+        except:
+            print ("There was an error while publishing the data.")
+        sleep(LOOP_INTERVAL)
+except KeyboardInterrupt:
+    print(" :Quitting!")
+finally:
+    exit()
